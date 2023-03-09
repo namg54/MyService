3d0e05cd78044605a8d105af8b0164b4198b3930
namg54 <79947165+namg54@users.noreply.github.com>
2023-03-09 05:49:32 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.ServiceReference1;
+
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestServiceSoapClient soapClient = new TestServiceSoapClient();
+            //TransferTest2 t2 = new TransferTest2();
+            List<TransferTest1> t2 = new List<TransferTest1>(soapClient.queueTest().ToList());
+            foreach ( var item in t2)
+            {
+                Console.WriteLine(item.Id + "/"+item.Name+"\n");
+            }
+            //Console.WriteLine(soapClient.queueTest());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyService/TransferTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyService
+{
+    public class TransferTest1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
